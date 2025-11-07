256ec65467991b87185d0d139868f0cb0822cb44
ivaSuker <iva.suker@fer.unizg.hr>
2025-11-07 17:39:38 +0100

    dodani komentari

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,13 @@
 import { useAuth } from '../auth/AuthContext'
 
 export default function Login() {
-  const { signIn } = useAuth()
-  return (
-    <div className="max-w-md mx-auto p-8 text-center">
+  const { signIn } = useAuth()  //uzimamo funkciju signIn koje pokrece OAuth2.0 prijavu
+  return (  //sve iza returna se prikazuje na ekranu
+    //centriran sadrzaj
+    <div className="max-w-md mx-auto p-8 text-center">  
+    {/*naslov stranice*/}
       <h1 className="text-3xl font-bold mb-6">Prijava</h1>
+      {/* gumb koji pokrece prijavu*/}
       <button onClick={signIn} className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-lg">
         Prijavi se putem Googlea
       </button>
