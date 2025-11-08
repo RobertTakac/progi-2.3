06cea2edb4059c0f00d07a5e3a68c155a0c956df
franmatijevic <fm55313@fer.hr>
2025-11-08 16:55:20 +0100

    app prop#2
    dodane stvari u application.property

--- backend/GearShare/src/main/java/com/patuljci/gearshare/service/JwtService.java
+++ backend/GearShare/src/main/java/com/patuljci/gearshare/service/JwtService.java
@@ -4,10 +4,11 @@ package com.patuljci.gearshare.service;
 import lombok.Value;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class JwtService {
-    @Value("${security.jwt.secret-key")
+    @Value("${security.jwt.secret-key}")
     private String secretKey;
-    @Value("${security.jwt.expiration-time")
+    @Value("${security.jwt.expiration-time}")
     private long jwtExpiration;
 }
