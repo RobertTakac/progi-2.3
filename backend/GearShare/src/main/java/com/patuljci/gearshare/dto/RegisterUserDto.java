6ee8bcaa46e9a09fd9d7d51b977164b18db7d4fa
Luka Barsic <lukabara10@gmail.com>
2025-11-08 20:23:12 +0100

    RegisterUserDto and dto class creation  #2
    RegisterUserDto and dto class creation  #2

---
+++ backend/GearShare/src/main/java/com/patuljci/gearshare/dto/RegisterUserDto.java
@@ -0,0 +1,12 @@
+package com.patuljci.gearshare.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+    private String email;
+    private String password;
+    private String username;
+}
