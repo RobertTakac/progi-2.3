516c7ae85f5185e00554f2464d2da05e9bcea2ba
franmatijevic <fm55313@fer.hr>
2025-11-08 16:21:49 +0100

    env file
    Dodan .env file

--- backend/GearShare/src/main/java/com/patuljci/gearshare/controller/MerchantController.java
+++ backend/GearShare/src/main/java/com/patuljci/gearshare/controller/MerchantController.java
@@ -1,4 +1,15 @@
 package com.patuljci.gearshare.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MerchantController {
+
+    @RequestMapping(method=RequestMethod.POST, value="/")
+    public void createAdvertisement(@RequestBody oglas oglas){
+
+    }
+
 }
