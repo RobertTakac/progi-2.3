1185507370d5b26616f01bf9ff00e6e8aa5e341f
Luka Barsic <lukabara10@gmail.com>
2025-11-08 20:24:59 +0100

    VerifyUserDto and LoginUserDto files #2
    VerifyUserDto and LoginUserDto files #2

---
+++ backend/GearShare/src/main/java/com/patuljci/gearshare/dto/VerifyUserDto.java
@@ -0,0 +1,11 @@
+package com.patuljci.gearshare.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VerifyUserDto {
+    private String email;
+    private String verificationCode;
+}
