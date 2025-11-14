c5540505137156c55f963bbbcdc0f847fba60bb9
Luka Barsic <lukabara10@gmail.com>
2025-11-14 17:43:02 +0100

    #4
    bug fix

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,7 +1,13 @@
 import logo from "./logo.svg";
 import CyclingImage from "./cycling.jpg";
+import kayak from "./kajak.avif";
+import diving from "./ronjenje.jpg";
+import racket from "./reket.jpeg";
 
 export const assets = {
     logo,
-    CyclingImage
+    CyclingImage,
+    kayak,
+    diving,
+    racket
 }
