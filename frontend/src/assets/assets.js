3ee2b6d348d9cf5489cd5c3c12e6bde139620a5a
Andrija <ab53822@fer.hr>
2026-01-13 21:33:19 +0100

    dodani buttoni za google auth

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,8 +1,8 @@
 import logo from "./logo.svg";
 import CyclingImage from "./cycling.jpg";
 import kayak from "./kajak.avif";
-import diving from "./ronjenje.jpeg";
-import racket from "reket.jpeg";
+import diving from "./ronjenje.jpg";
+import racket from "./reket.jpeg";
 
 export const assets = {
     logo,
