e040ec4eda28e767f4bc250c0097f893f0e4099a
ivaSuker <iva.suker@fer.unizg.hr>
2026-01-22 00:17:30 +0100

    Add roles to frontend and backend; Cleanup/simplification

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
 import "./Navbar.css";
+
 
 const Navbar = ({ currentUser, openLoginModal, handleSignOut }) => {
 
   const baseLinks = [
     { name: "Home", path: "/" },
-    { name: "Lokacije", path: "/lokacije" },
-    ...(currentUser ? [{ name: 'Ponuda', path: '/ponuda' }] : [])
+    { name: "Lokacije", path: "/lokacije" }
   ];
 
-  let navigationLinks = [...baseLinks];
-
   if (currentUser) {
-    if (currentUser.type === "merchant") {
-      navigationLinks.push({ name: "Moji Oglasi", path: "/moji-oglasi" });
+    baseLinks.push({ name: 'Ponuda', path: '/ponuda' });
+    if (currentUser.type === "merchant" || currentUser.type === "admin") {
+      baseLinks.push({ name: "Moji oglasi", path: "/moji-oglasi" });
     }
   }
+
+  let navigationLinks = [...baseLinks];
 
   return (
     <>
